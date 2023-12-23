61f9905fb55df4f04a2c6fd464c388c66fef4d07
abhisek0507 <abhisek.singh@paymob.com>
2023-12-24 02:36:25 +0530

    added fake profile utility and added profile to have custom msnsgement

--- src/profiles/management/commands/loader.py
+++ src/profiles/management/commands/loader.py
@@ -1,6 +1,9 @@
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
+user = get_user_model()
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
         print(options)
